43abb1e8067844d77a32578e2bb06ed41fd32320
Ben Daniels <benjamindaniels21@gmail.com>
2022-10-01 19:03:57 -0400

    finished w js logic

--- app.js
+++ app.js
@@ -33,3 +33,62 @@ const reviews = [
 /* what data do we want to access? 
 We need image, name, job, and the text...use ID to display the current reviewer
 */
+
+//select items
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+//starting item
+let currentItem = 0;
+
+// load initial item
+
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+// show person based on item
+
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//show next person
+
+nextBtn.addEventListener("click", function () {
+  if (currentItem === 3) {
+    currentItem = 0;
+  } else {
+    currentItem++;
+  }
+  showPerson();
+});
+
+// show previous person
+
+prevBtn.addEventListener("click", function () {
+  if (currentItem === 0) {
+    currentItem = 3;
+  } else {
+    currentItem--;
+  }
+  showPerson();
+});
+
+// show random person
+
+randomBtn.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
